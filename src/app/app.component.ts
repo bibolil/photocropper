7ca2a325f7d739de917e9098197af3008cb429b7
Bilel Taboubi <bileltaboubi20@gmail.com>
2023-08-25 19:43:00 +0100

    cropped can go to max size of the pic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,23 @@ export class AppComponent {
   title = 'photocropper';
   imgChangedEvent: any = "";
   cropImagePreview: any="";
-
+  imgWidth: number = 0;
+  imgHeight: number = 0;
+  img: any="";
   
   fileChangeEvent(event: any): void {
     this.imgChangedEvent = event;
-    console.log(event);
+    this.img=event.target.files[0];
+    var img = new Image();
+      img.src=URL.createObjectURL(event.target.files[0])
+      img.onload = () => {
+          this.imgWidth = img.width;
+          this.imgHeight = img.height;
+          console.log(this.imgHeight)
+
+      };
   }
+
   imageCropped(event: ImageCroppedEvent) {}
 
 
