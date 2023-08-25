fef379dac726b5836f40ea5de9dfc0a73324377f
Bilel Taboubi <bileltaboubi20@gmail.com>
2023-08-25 18:32:58 +0100

    Last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
-import {ImageCropperModule} from 'ngx-image-cropper';
-
+import { ImageCroppedEvent, ImageCropperModule } from 'ngx-image-cropper';
+import 'hammerjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
   title = 'photocropper';
-  imgChangedEvent: any = '';
+  imgChangedEvent: any = "";
+  cropImagePreview: any="";
+
+  
   fileChangeEvent(event: any): void {
     this.imgChangedEvent = event;
+    console.log(event);
   }
+  imageCropped(event: ImageCroppedEvent) {}
+
+
+    loadImageFailed() {
+      alert("Image failed to load");
+    }
+
+    initCropper() {
+
+    }
+    imgLoad() {
+
+    }
+    cropImg(e: ImageCroppedEvent) {
+      this.cropImagePreview = e.objectUrl; 
+    }
 }
